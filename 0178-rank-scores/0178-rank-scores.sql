bcb162518a1895417ae087181f1963f9ba84f907
omm jyoti puthal <ommjyotiputhal61@gmail.com>
2026-08-26 19:53:18 +0530

    Time: 349 ms (49.44%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,3 +1,12 @@
-SELECT score,
-       DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
-FROM Scores;
+-- SELECT score,
+--        DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
+-- FROM Scores;
+
+
+
+
+
+
+select score,
+    DENSE_RANK() OVER(order by score desc) as' rank'
+ from Scores;
